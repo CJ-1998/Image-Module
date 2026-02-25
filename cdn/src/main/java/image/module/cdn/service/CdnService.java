2d7884d29d9ed1ef2f3cc4f98aef467a1b1503b7
CJ <choijun0627@naver.com>
2026-02-25 16:39:30 +0900

    [IMAGE-15]  죽은 코드인 비동기 삭제
    @Async 어차피 비동기로 동작하지 않아서 삭제함

--- cdn/src/main/java/image/module/cdn/service/CdnService.java
+++ cdn/src/main/java/image/module/cdn/service/CdnService.java
@@ -65,7 +65,6 @@ public class CdnService {
         return imageResponseDto;
     }
 
-    @Async
     public ImageResponseDto getImageInfo(String fileLocation) throws IOException {
         ImageResponseDto imageResponseDto = new ImageResponseDto();
 
